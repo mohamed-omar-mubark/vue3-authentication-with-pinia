4f87d51cf0c7579836636edfe47aabf7090df381
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2023-04-23 15:27:22 +0200

    style(public): :lipstick: add navigation style

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
+  <navigation />
   <RouterView />
 </template>
+
+<script setup>
+import Navigation from "@/modules/public/layout/components/Navigation.vue";
+</script>
---
+++ src/modules/public/layout/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="bg-gray-800">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-center space-x-4">
+        <router-link
+          :to="{ name: 'Home' }"
+          class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+          aria-current="page"
+        >
+          Home
+        </router-link>
+
+        <router-link
+          :to="{ name: 'Login' }"
+          class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+          aria-current="page"
+        >
+          Login
+        </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
